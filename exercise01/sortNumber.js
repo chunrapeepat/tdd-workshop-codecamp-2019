ae49c0797aebc27324a96faf76e10a51a83687cf
chunza2542 <chunza2542@hotmail.com>
2019-12-22 21:27:02 +0700

    Add Exercise 01

---
+++ exercise01/sortNumber.js
@@ -0,0 +1,15 @@
+function sortNumber(a, b, c) {
+  const result = [a, b, c];
+  if (result[0] > result[2]) {
+    [result[2], result[0]] = [result[0], result[2]];
+  }
+  if (result[0] > result[1]) {
+    [result[1], result[0]] = [result[0], result[1]];
+  }
+  if (result[1] > result[2]) {
+    [result[1], result[2]] = [result[2], result[1]];
+  }
+  return result;
+}
+
+module.exports = sortNumber;
