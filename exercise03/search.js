c6b3fd4407a57540502e059141bd55e1e4b304d5
chunza2542 <chunza2542@hotmail.com>
2019-12-23 22:38:06 +0700

    Clean Exercise03

--- exercise03/search.js
+++ exercise03/search.js
@@ -1,8 +1,5 @@
 function search(arr, elem) {
-  for (let i = 0; i < arr.length; ++i) {
-    if (arr[i] === elem) return i;
-  }
-  return -1;
+  // TODO: Implement Code
 }
 
 module.exports = search;
