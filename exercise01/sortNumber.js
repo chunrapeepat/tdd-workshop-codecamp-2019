e8c43cbb47e812eb0bf19b1644e10a7884d0bbe8
chunza2542 <chunza2542@hotmail.com>
2019-12-23 22:34:47 +0700

    Clean Exercise01

--- exercise01/sortNumber.js
+++ exercise01/sortNumber.js
@@ -1,15 +1,5 @@
 function sortNumber(a, b, c) {
-  const result = [a, b, c];
-  if (result[0] > result[2]) {
-    [result[2], result[0]] = [result[0], result[2]];
-  }
-  if (result[0] > result[1]) {
-    [result[1], result[0]] = [result[0], result[1]];
-  }
-  if (result[1] > result[2]) {
-    [result[1], result[2]] = [result[2], result[1]];
-  }
-  return result;
+  // TODO: Implement Function
 }
 
 module.exports = sortNumber;
