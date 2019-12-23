903efd02e97172e4435327b08be24f692e9fcb61
chunza2542 <chunza2542@hotmail.com>
2019-12-23 12:25:27 +0700

    Refactor Code

--- exercise04/eval.js
+++ exercise04/eval.js
@@ -1,12 +1,12 @@
 function evaluate(expr) {
   expr = expr.split(" ");
-  let result = +expr[0];
+  let result = parseInt(expr[0]);
   for (let i = 1; i < expr.length - 1; i += 2) {
     if (expr[i] == '+') {
-      result += +expr[i + 1];
+      result += parseInt(expr[i + 1]);
     }
     if (expr[i] == '-') {
-      result -= +expr[i + 1];
+      result -= parseInt(expr[i + 1]);
     }
   }
   return result;
