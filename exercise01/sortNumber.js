b65ce2ee111389b9da89d7969b5dff8da6f0e80a
Chun Rapeepat <chunza2542@hotmail.com>
2024-03-25 12:34:00 +0700

    Update sortNumber.js

--- exercise01/sortNumber.js
+++ exercise01/sortNumber.js
@@ -3,11 +3,11 @@ function sortNumber(a, b, c) {
   if (result[0] > result[2]) {
     [result[2], result[0]] = [result[0], result[2]];
   }
-  if (result[0] > result[1]) {
-    [result[1], result[0]] = [result[0], result[1]];
-  }
   if (result[1] > result[2]) {
     [result[1], result[2]] = [result[2], result[1]];
+  }
+  if (result[0] > result[1]) {
+    [result[1], result[0]] = [result[0], result[1]];
   }
   return result;
 }
